adb6df72cbb60610e2fe8fa7ee2cece33cbb55cf
PrachitGharat <prachitgharat00@gmail.com>
2023-06-12 17:13:33 +0530

    Api Bind For AddPhoto

--- src/components/AddPhoto/AddPhoto.tsx
+++ src/components/AddPhoto/AddPhoto.tsx
@@ -1,13 +1,51 @@
-import { Container } from '@mui/material'
+import { Button, Container,TextField,Typography} from '@mui/material'
 import React,{useState,useEffect} from 'react'
 import PageHeader from 'src/library/heading/pageHeader'
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { RootState } from 'src/store';
+import { toast } from 'react-toastify';
+import {IAddPhotoAlbumBody} from 'src/Interface/Admin/IAddPhoto'
+import {getAddPhoto} from 'src/requests/Admin/RequestAddPhoto'
 
 function AddPhoto  ()  {
+
+
+    const [title, setTitle] = useState('');
+    const onSubmit = () => {
+        console.log({ title })
+    
+      }
+
+    const GetAddPhoto: any = useSelector(
+        (state: RootState) => state.AddPhoto.AddPhotoAlbum
+    );
+    const dispatch = useDispatch();
+    console.log(GetAddPhoto,"AddPhoto")
+
+    const AddPhoto: IAddPhotoAlbumBody =
+    {
+        "Title":"Agnipariksha",
+        "Class":"firstStandard",
+        "AlbumDate":"05/27/2023",
+        "FacebookLink":"httplocalhost45",
+        "CreatedBy":101
+    }
+    useEffect(() => {
+        toast.success(GetAddPhoto)
+        dispatch(getAddPhoto(AddPhoto));
+      }, [])
     
 
   return (
     <Container>
         <PageHeader heading={'AddPhoto'} />
+        <TextField value={title} onChange={(e)=>{setTitle(e.target.value)}}  
+      label={'Title'} />
+       <Typography> Link</Typography>
+      <TextField type='date' />
+      <br></br>
+      <Button onClick={onSubmit}>Submit</Button>
     </Container>
   )
 }
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -6,13 +6,16 @@ import ChangePasswordslice from 'src/requests/ChangePassword/ChangePassword'
 import Homeworkslice from 'src/requests/Student/Homework/RequestHomework'
 import Viewphotoslice from 'src/requests/Student/Viewphoto/RequestViewphoto'
 import AddHomeWorkSlice from 'src/requests/Teacher/RequestAddHomeWork';
+import AddPhotoSlice from 'src/requests/Admin/RequestAddPhoto'
+
 const rootReducer = combineReducers({
     Holidays:Holidaysslice,
     SchoolList: SchoolListslice,
     ChangePassword:ChangePasswordslice,
     AddHomeWork: AddHomeWorkSlice,
     HomeWork: Homeworkslice,
-    Viewphoto: Viewphotoslice
+    Viewphoto: Viewphotoslice,
+    AddPhoto:AddPhotoSlice
 });
 
 export default rootReducer;
