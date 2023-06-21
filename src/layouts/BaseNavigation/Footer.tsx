e9601be9df683b6a372ed2792953f863cd098993
renukaadwe <renukaadwe25@gmail.com>
2023-06-21 10:24:36 +0530

    Footer addPath

--- src/layouts/BaseNavigation/Footer.tsx
+++ src/layouts/BaseNavigation/Footer.tsx
@@ -1,28 +1,36 @@
-import { Paper ,Box ,Grid , Card ,Typography, IconButton} from '@mui/material'
+import { Paper ,Box , IconButton} from '@mui/material'
 import React from 'react';
 import { Link } from 'react-router-dom'
 import HomeIcon from '@mui/icons-material/Home';
-import { useSelector } from 'react-redux';
-import { RootState } from 'src/store';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import ChromeReaderModeIcon from '@mui/icons-material/ChromeReaderMode';
 
 function Footer() {
 
-  const UserType = localStorage.getItem('UserType');
+  const UserType = sessionStorage.getItem('UserType');
+  
   return (
     <div>
    <Paper square>
-    <Box sx={{   zIndex: "9999" }}>
+    <Box sx={{zIndex: "9999" ,textAlign:"center"}}>
       
-      <Link to={`/${location.pathname.split('/')[1]}/landing/landing`} style={{ textDecoration: 'none' }}>
-            <Card sx={{textAlign:"center"}}>
-              {UserType === '3' && "hd"}
-          <IconButton >
+      {/* <Link to={`/${location.pathname.split('/')[1]}/landing/landing`} style={{ textDecoration: 'none' }}>
+         <IconButton >
           <HomeIcon fontSize='large'/>
           </IconButton>
-          
-            </Card>
+          </Link> */}
+            <Link to={`/${location.pathname.split('/')[1]}/Student/Homework`} style={{ textDecoration: 'none' }}>
+            {UserType === '3' && <IconButton><MenuBookIcon /> </IconButton>  }
             </Link>
-   
+
+            <Link to={`/${location.pathname.split('/')[1]}/Student/ViewPhotoAlbum`} style={{ textDecoration: 'none' }}>
+            {UserType === '3' && <IconButton><AddPhotoAlternateIcon /> </IconButton>  }
+            </Link>
+
+            {/* <Link to={`/${location.pathname.split('/')[1]}/Student/AddHomeWork`} style={{ textDecoration: 'none' }}>
+            {UserType === '2' && <IconButton><ChromeReaderModeIcon /> </IconButton>  }
+            </Link> */}
       </Box>
       </Paper>
     </div>
