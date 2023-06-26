588b5a0cd8a48e70774b6fb5c372fde1b6568e1e
PrachitGharat <prachitgharat00@gmail.com>
2023-06-26 13:24:04 +0530

    Rwminder Radio Button,TextAreaAutoSize for Comment

--- src/components/FollowUp/FollowUp.tsx
+++ src/components/FollowUp/FollowUp.tsx
@@ -1,4 +1,4 @@
-import { Card, Container, TextField ,Grid, Button} from '@mui/material'
+import { Card, Container, TextField ,Grid, Button, Typography} from '@mui/material'
 import React, { useState ,useEffect} from 'react'
 import PageHeader from 'src/library/heading/pageHeader'
 import Radio from '@mui/material/Radio';
@@ -8,6 +8,8 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import SelectedCard from 'src/library/Card/SelectedCard';
 import CheckUnCheckList from 'src/library/Card/CheckUnCheckList';
+import { TextareaAutosize } from '@mui/material';
+
 
 
 const FollowUp = () => {
@@ -18,10 +20,15 @@ const[phonenumber1, setPhoneNumber1] = useState("");
 const[mothername, setMotherName] = useState("");
 const[phonenumber2, setPhoneNumber2] = useState("");
 const[email, setEmail] = useState("");
- const[itemList , setItemList] =useState([{Id:1  ,Name:"yes" , Value:1 , IsActive:false} ,{Id:2  ,Name:"No" , Value:2 , IsActive:false}, {Id:3  ,Name:"Did Not Connect" , Value:3 , IsActive:false}])
+ const[itemList , setItemList] =useState([{Id:1  ,Name:"yes" , Value:1 , IsActive:false} ,{Id:2  ,Name:"No" , Value:2 , IsActive:false}, {Id:3  ,Name:"Did Not Connect" , Value:3 , IsActive:false}]);
+ const[reminderitemlist,setReminderItemList]=useState([{Id:1,Name:"2 days",Value:1,IsActive:false},{Id:2,Name:"7 days",Value:2,IsActive:false},{Id:3,Name:"10 days",IsActive:false},{Id:4,Name:"1 month",Value:4,IsActive:false}]);
+ const[comment,setComment]=useState("");
 
 const clickItem=(value)=>{
     setItemList(value)
+}
+const clickReminderItem=(value)=>{
+  setReminderItemList(value)
 }
   return (
     <div>
@@ -44,11 +51,20 @@ const clickItem=(value)=>{
            <TextField value={phonenumber2} onChange={(e)=>{setPhoneNumber2(e.target.value)}} label={'Phone Number'}/>
             </Grid>
         </Grid>
-
         <TextField value={email} onChange={(e)=>{setEmail(e.target.value)}}  label={'Email'}/>
-        
+        <br/>
+        <br/>
+        <Typography>Status of Call</Typography>
        <CheckUnCheckList ItemList={itemList} clickItem={clickItem}/>
+       <br></br>
+       <Typography>Reminder</Typography>
+       <CheckUnCheckList ItemList={reminderitemlist} clickItem={clickReminderItem}/>
+      
+       {/* <TextField value={comment} onChange={(e)=>{setComment(e.target.value)}}  label={'Comment'} multiline
+          maxRows={6}/> */}
+          <TextareaAutosize name="Outlined" placeholder="Comment" minRows={4} style={{width:"100%"}}/>
 <br></br>
+
     <Button>Save</Button>
         
         </Card>
