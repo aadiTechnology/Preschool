afb12eade1575a0c40c3929b51ecf4f6e6eaeafc
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2024-04-17 18:55:04 +0530

    src-dlt

--- src/Interface/ChangePassword.ts
+++
@@ -1,9 +0,0 @@
-export interface IChangePasswordBody {
-    UserId: string;
-    NewPassword: string;
-    OldPassword: string;
-};
-
-export interface IChangePasswordResult {
-    ChangePasswordResult:string
-}
